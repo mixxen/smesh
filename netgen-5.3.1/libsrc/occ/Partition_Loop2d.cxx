69bc48a37754b4eff2964f4ee9527883252e07eb
Alexander Cabello <alexander.cabello@gmail.com>
2016-04-23 10:35:38 -1000

    Change PI to M_PI.

--- netgen-5.3.1/libsrc/occ/Partition_Loop2d.cxx
+++ netgen-5.3.1/libsrc/occ/Partition_Loop2d.cxx
@@ -210,7 +210,7 @@ static Standard_Boolean  SelectEdge(const BRepAdaptor_Surface& Surf,
     Cc->D1(uc, PC, CTg1);
     if (!isForward) CTg1.Reverse();
 
-    Standard_Real anglemin = 3 * PI, tolAng = 1.e-8;
+    Standard_Real anglemin = 3 * M_PI, tolAng = 1.e-8;
 
     // select an edge whose first derivative is most left of CTg1
     // ie an angle between Tg1 and CTg1 is least
@@ -231,10 +231,10 @@ static Standard_Boolean  SelectEdge(const BRepAdaptor_Surface& Surf,
       C->D1(u, P, Tg1);
       if (!isForward) Tg1.Reverse();
 
-      // -PI < angle < PI
+      // -M_PI < angle < PI
       Standard_Real angle = Tg1.Angle(CTg1);
 
-      if (PI - Abs(angle) <= tolAng)
+      if (M_PI - Abs(angle) <= tolAng)
       {
         // an angle is too close to PI; assure that an angle sign really
         // reflects an edge position: +PI - an edge is worst,
